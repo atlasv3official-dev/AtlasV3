0fc1c41bb56ab6ca6b388a0f7af0002d446a1d00
collatzcoder <130808304+collatzcoder@users.noreply.github.com>
2026-01-29 18:14:13 -0600

    buig list

--- server_scripts/servermain.js
+++ server_scripts/servermain.js
@@ -11,33 +11,74 @@ let itemids=[
     'mekanism:teleporter',
     'mekanism:teleporter_frame',
     'tacz:ammo',
-    'createbigcannons:flak_autcannon_round'
-    //flak shells above from
-    //mek storage
-    //ae2 spatial
-    //ae2 autocrafting
-    //gravitron
-    //zero contact stuff make recipes
-    //
+    'createbigcannons:flak_autcannon_round',
+    'wrbdrones:shahed136',
+    'wrbdrones:radio',
+    'zerocontact:steel_plate',
+    'zerocontact:si_plate',
+    'zerocontact:bc_plate',
+    'zerocontact:ceramic_plate',
+    'mekanism:qio_drive',
+    'mekanism:qio_drive_hyper_dense',
+    'mekanism:qio_drive_time_dilating',
+    'mekanism:qio_drive_supermassive',
+    
 ]
-
+let superbomits=[
+    'superbwarfare:mortar_deployer',
+    'superbwarfare:mortar_barrel',
+    'superbwarfare:mortar_base_plate',
+    'superbwarfare:mortar_bipod',
+    'superbwarfare:potion_mortar_shell',
+    'superbwarfare:mortar_shell',
+    'superbwarfare:drone',
+    'superbwarfare:monitor',
+    'superbwarfare:hand_grenade',
+    'superbwarfare:rgo_grenade',
+    'superbwarfare:smoke_grenade',
+    'superbwarfare:taser',
+    'superbwarfare:rpg_rocket_standard',
+    'superbwarfare:c4_bomb',
+    'superbwarfare:claymore_mine',
+    'superbwarfare:blu_43_mine',
+    'superbwarfare:',
+    
+]
 ServerEvents.recipes(event =>{
 
-for(i=0;i<itemids.length;i++){
+for(var i=0;i<itemids.length;i++){
     event.remove({output: itemids[i] })
 }
+
+event.remove({mod: vs_tournament})
+//note allow ship assembler
+
+for(var i=0;i<superbomits.length;i++){
+    event.remove({ not: { output:superbomits[i] }, mod: superbwarfare })
+}
+
 
 });
 
 let gunids=[]
 TaCZServerEvents.gunIndexLoad((event) => {
 const id = event.getId().toString();
-for(i=0;i<gunids.length;i++){
+for(var i=0;i<gunids.length;i++){
    if(id==gunids[1]){
        return event.removeGun();
    }
 }
-return event.removeGun();
     
 });
-
+//MAKE RECIPES FOR
+//flak shells
+    //mek storage
+    //ae2 spatial
+    //ae2 autocrafting
+    //gravitron
+    //zero contact plate steel si bc ceramic (process, diamond+quartz+2 zero contact steel sheets)
+    //all zero contact armor
+    //zero contact helmets (steel helmet+zero contact steel sheets)
+    //controlcraft camera (exposure camera+mech bearing)
+    //controlcraft camera link (exposure camera+create link)
+    //air thrusters?
